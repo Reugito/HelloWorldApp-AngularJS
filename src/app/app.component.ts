562356248c9f32035148cebd22093910107d07e1
Reugito <raosahebdhotreedu@gmail.com>
2021-09-07 13:27:04 +0530

    added onInput and onclick functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
+  imgUrl = "../assets/blzlogo.png";
+  url = "https://www.bridgelabz.com/"
+  userName = "";
+  nameError="";
+  ngOnInit(): void{
+    this.title = "Hello from bridgeLabz"
+  }
 
- 
+  onClick($event: any){
+    console.log("save button is clicked", $event);
+    window.open(this.url, "-blanck")
+  }
+
+  onInput($event: any){
+    console.log("change event occured!", $event.data);
+    const nameRegexp = RegExp('^[A-z][a-zA-z\\s]{2,}$');
+    if(nameRegexp.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!;"
+  }
 }
